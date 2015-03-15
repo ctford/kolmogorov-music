2af26530335864a67575c56f92574bd120854e3d
Chris Ford <christophertford@gmail.com>
2015-03-15 19:49:11 +0000

    Juxt makes it more succinct.

--- src/kolmogorov_music/champernowne.clj
+++ src/kolmogorov_music/champernowne.clj
@@ -1,8 +1,7 @@
 (ns kolmogorov-music.champernowne)
 
 (defn decompose [n]
-  (let [remainder (mod n 10)
-        quotient (quot n 10)]
+  (let [[remainder quotient] ((juxt mod quot) n 10)]
     (if (pos? quotient)
       (conj (decompose quotient) remainder)
       [remainder])))
