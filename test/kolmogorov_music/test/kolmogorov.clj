6d0dc93c234398622adc011cfd5c36371e48750c
Chris Ford <christophertford@gmail.com>
2015-03-15 17:17:46 +0000

    Test that the count works with nested sexprs.

--- test/kolmogorov_music/test/kolmogorov.clj
+++ test/kolmogorov_music/test/kolmogorov.clj
@@ -3,13 +3,17 @@
             [midje.sweet :refer :all]))
 
 (defn foo [x] (inc x))
-(def bar (comp foo foo))
+(defn bar [x] (+ (inc x) x))
+(def baz (comp foo foo))
 
 (fact "Kolmogorov complexity is how many symbols a definition comprises."
   (kolmogorov/complexity foo) => 2)
 
+(fact "The symbol count includes nested sexprs."
+  (kolmogorov/complexity bar) => 4)
+
 (fact "The symbol count is recursive within the current namespace."
-  (kolmogorov/complexity bar) => 7)
+  (kolmogorov/complexity baz) => 7)
 
 (fact "Symbols outside the current namespace are considered atoms."
   (kolmogorov/complexity inc) => 0)
