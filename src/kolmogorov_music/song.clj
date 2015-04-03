94da104df7c8b3432f16c905dc84bede0dc2886a
Chris Ford <christophertford@gmail.com>
2015-04-03 09:38:44 +0100

    Simple encoding of Champernowne into an infinite piece.

--- src/kolmogorov_music/song.clj
+++ src/kolmogorov_music/song.clj
@@ -4,47 +4,103 @@
             [leipzig.scale :as scale]
             [leipzig.live :as live]
             [leipzig.chord :as chord]
-            [leipzig.temperament :as temperament]))
+            [leipzig.temperament :as temperament]
+            [kolmogorov-music.champernowne :as champernowne]))
+
+(def master-volume 0.1)
 
 ; Instruments
-(definst bass [freq 110 volume 1.0]
+(definst buzz [freq 110 vol 1.0]
   (-> (saw freq)
-      (* (env-gen (perc 0.1 0.4) :action FREE))
-      (* volume)))
+      (+ (sin-osc (* 1/2 freq)))
+      (rlpf (* 2 440) 1/9)
+      (* (env-gen (perc 0.01 0.3) :action FREE))
+      (* vol master-volume)))
 
-(definst organ [freq 440 dur 1 volume 1.0]
+(definst sing [freq 110 dur 1.0 vol 1.0]
+  (-> (sin-osc freq)
+      (+ (sin-osc (* 3 freq)))
+      (+ (* 1/3 (sin-osc (* 2 freq))))
+      (+ (* 1/3 (sin-osc (* 1/2 freq))))
+      (rlpf (line:kr 3000 500 dur) 1/3)
+      (clip2 0.3)
+      (* (env-gen (adsr 0.05 (* dur 1/3) 0.5) (line:kr 1 0 dur) :action FREE))
+      (* vol master-volume)))
+
+(definst organ [freq 440 dur 1 vol 1.0]
   (-> (square freq)
-      (* (env-gen (adsr 0.01 0.8 0.1) (line:kr 1 0 dur) :action FREE))
-      (* 1/4 volume)))
+      (+ (sin-osc (* 1/2 freq)))
+      (* (sin-osc 3))
+      (lpf 3000)
+      (* (env-gen (perc (* 1/4 dur) (* 3/4 dur)) :action FREE))
+      (* 1/3 vol master-volume)))
+
+(defn evens [[x y & xs]]
+  (cons x (lazy-seq (evens xs))))
+
+(defn odds [[x y & xs]]
+  (cons y (lazy-seq (odds xs))))
+
+(defn split [xs]
+  [(evens xs) (odds xs)])
+
+(comment
+(fx-reverb)  
+(fx-chorus)
+  )
 
 ; Arrangement
-(defmethod live/play-note :bass [{hertz :pitch}] (bass hertz))
+(defmethod live/play-note :bass [{hertz :pitch}] (buzz hertz))
+(defmethod live/play-note :melody [{hertz :pitch seconds :duration}] (sing hertz seconds))
 (defmethod live/play-note :accompaniment [{hertz :pitch seconds :duration}] (organ hertz seconds))
 
 ; Composition
-(def progression [0 0 3 0 4 0])
+(def melody
+  (let [[durations pitches] (split (champernowne/word 7))]
+    (->> 
+      (phrase (map #([4 2 1 1/2 1/4] (mod % 5)) durations) pitches)
+       (where :part (is :melody)))))
 
-(defn bassline [root]
-  (->> (phrase (cycle [1 1/2 1/2 1 1]) [0 -3 -1 0 2 0 2 3 2 0])
-       (where :pitch (scale/from root))
+(def chords
+  (->> (champernowne/word 7)
+       (map #(-> chord/seventh (chord/root %)))))
+
+(def accompaniment
+  (->> chords
+       (phrase (repeat 4)) 
+       (where :part (is :accompaniment))))
+
+(def bass
+  (->> chords
+       (mapcat vals)
+       (phrase (cycle [2 1 1])) 
        (where :pitch (comp scale/lower scale/lower))
        (where :part (is :bass))))
-
-(defn accompaniment [root]
-  (->>
-    (phrase [8] [(-> chord/seventh (chord/root root))])
-    (where :part (is :accompaniment))))
 
 ; Track
 (def track
   (->>
-    (mapthen bassline progression)
-    (with (mapthen accompaniment progression))
+    melody
+    (with bass)
+    (with accompaniment)
     (where :pitch (comp temperament/equal scale/A scale/minor))
-    (where :time (bpm 90))
-    (where :duration (bpm 90))))
+    (where :time (bpm 100))
+    (where :duration (bpm 100))))
 
 (comment
   ; Loop the track, allowing live editing.
   (live/jam (var track))
 )
+
+(comment
+  two voices?
+  difference ranges?
+  difference instruments?
+  difference pans?
+
+  rest
+  change mode 
+  change pitch
+  change duration
+  sharpen
+  )
