04a7f926a77b8e09df9eec0cfee9e95f0f08ec26
Chris Ford <christophertford@gmail.com>
2015-04-09 14:15:53 +0100

    Check the encoding directly.

--- test/kolmogorov_music/test/song.clj
+++ test/kolmogorov_music/test/song.clj
@@ -3,6 +3,14 @@
             [midje.sweet :refer :all]
             [leipzig.melody :as leipzig]))
 
+(fact "code encodes each note of a melody into four digits that determine pitch and duration."
+  (->> (leipzig/phrase [2/3 1/3] [0 4]) song/code) => 23001322)
+
+(fact "Multiple parts are multiplexed, based on which part is furthest behind."
+  (->> (leipzig/phrase [1 1 1 1] [0 1 2 3])
+       (with (leipzig/phrase [2 2] [4 5]))
+       song/code) => 212211001101212311111112)
+
 (fact "(comp code decode) is an identity for a single part."
   (let [melody (leipzig/phrase [3/3 3/3 2/3 1/3 3/3]
                                [  0   0   0   1   2])]
