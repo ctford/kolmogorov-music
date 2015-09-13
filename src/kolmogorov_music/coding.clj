966101b09987fd57fc9d66683e84b5de355b80c3
Chris Ford <christophertford@gmail.com>
2015-09-13 12:46:14 +0100

    Split coding into its own file.

---
+++ src/kolmogorov_music/coding.clj
@@ -0,0 +1,53 @@
+(ns kolmogorov-music.coding
+  (:require [leipzig.melody :refer :all]
+            [leipzig.scale :as scale]
+            [leipzig.live :as live]
+            [leipzig.chord :as chord]
+            [leipzig.temperament :as temperament]
+            [kolmogorov-music.instrument :as instrument]))
+
+(defn least [[x & xs :as v]]
+  (if (= x (apply min v))
+    0
+    (inc (least xs))))
+
+(defn synchronise [v]
+  (vec (repeat (count v) (apply max v))))
+
+(defn increment [v i n]
+  (update-in v [i] (partial + n)))
+
+(defn digit-shift [x n]
+  (apply * x (repeat n (biginteger 10))))
+
+(defn construct [a b c d]
+  (if (zero? (* a b))
+    {:duration (/ c (max d 1))}
+    {:duration (/ a b) :pitch (+ (digit-shift c 1) d)}))
+
+(defn decode*
+  [state [a b c d & digits :as remaining?]]
+  (when remaining?
+    (let [voice (least state)
+          time (get state voice)
+          note (assoc (construct a b c d) :time time)]
+      (->> digits
+           (decode* (-> state (increment voice (:duration note))))
+           lazy-seq
+           (cons note)))))
+
+(defn flatten-ratio [duration]
+  (let [d (if (ratio? duration) (denominator duration) 1)
+        n (* d duration)]
+    (+ (digit-shift n 1) d)))
+
+(defn code [[{:keys [duration pitch] :as remaining?} & notes]]
+  (if remaining?
+    (let [encoding (if pitch
+                     (+ (digit-shift (flatten-ratio duration) 2) pitch)
+                     (+ (flatten-ratio duration)))]
+      (+ (digit-shift encoding (* 4 (count notes))) (code notes)))
+    0))
+
+(defn decode [channels notes]
+  (decode* (vec (repeat channels 0)) notes))
--- src/kolmogorov_music/kolmogorov.clj
+++ src/kolmogorov_music/kolmogorov.clj
@@ -7,7 +7,8 @@
             [leipzig.live :as live]
             [leipzig.chord :as chord]
             [leipzig.temperament :as temperament]
-            [kolmogorov-music.instrument :as instrument]))
+            [kolmogorov-music.instrument :as instrument]
+            [kolmogorov-music.coding :as coding]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Air on a \G String ;;;
@@ -142,67 +143,6 @@
   [{hertz :pitch seconds :duration}]
   (when hertz (instrument/overchauffeur hertz seconds 0.02)))
 
-(defn least [[x & xs :as v]]
-  (if (= x (apply min v))
-    0
-    (inc (least xs))))
-
-(defn synchronise [v]
-  (vec (repeat (count v) (apply max v))))
-
-(defn increment [v i n]
-  (update-in v [i] (partial + n)))
-
-(defn digit-shift [x n]
-  (apply * x (repeat n (biginteger 10))))
-
-(defn construct [a b c d]
-  (if (zero? (* a b))
-    {:duration (/ c (max d 1))}
-    {:duration (/ a b) :pitch (+ (digit-shift c 1) d)}))
-
-(defn decode*
-  [state [a b c d & digits :as remaining?]]
-  (when remaining?
-    (let [voice (least state)
-          time (get state voice)
-          note (assoc (construct a b c d) :time time)]
-      (->> digits
-           (decode* (-> state (increment voice (:duration note))))
-           lazy-seq
-           (cons note)))))
-
-(defn flatten-ratio [duration]
-  (let [d (if (ratio? duration) (denominator duration) 1)
-        n (* d duration)]
-    (+ (digit-shift n 1) d)))
-
-(defn code [[{:keys [duration pitch] :as remaining?} & notes]]
-  (if remaining?
-    (let [encoding (if pitch
-                     (+ (digit-shift (flatten-ratio duration) 2) pitch)
-                     (+ (flatten-ratio duration)))]
-      (+ (digit-shift encoding (* 4 (count notes))) (code notes)))
-    0))
-
-(def row
-  (->> (phrase [3/3 3/3 2/3 1/3 3/3]
-               [0 0 0 1 2])
-       (then
-         (phrase [2/3 1/3 2/3 1/3 6/3]
-                 [2 1 2 3 4]))
-       (then
-         (phrase (repeat 1/3)
-                 [7 7 7 4 4 4 2 2 2 0 0 0]))
-       (then
-         (phrase [2/3 1/3 2/3 1/3 6/3]
-                 [4 3 2 1 0]))
-       (with (->> (phrase [8] [9]) (times 2)))
-       (with (->> (phrase [1 1 2] [-7 -3 -7]) (times 4)))
-       ;(with (->> (phrase (cycle [2/3 1/3]) [8 8 nil 8 nil 8 nil 7]) (times 4)))
-       (wherever :pitch, :pitch (comp scale/A scale/major scale/lower))
-       code))
-
 (def blurred-lines
   (let [riff (phrase (repeat 8 1/2) (concat [[nil nil]] (repeat 6 [2 4]) [[nil nil]]))
         accompaniment (->> (times 4 riff)
@@ -219,15 +159,12 @@
     (->>
       (with bass accompaniment)
       (wherever :pitch, :pitch (comp scale/A scale/major))
-      code)))
-
-(defn decode [channels notes]
-  (decode* (vec (repeat channels 0)) notes))
+      coding/code)))
 
 (defn track [start]
   (->>
     (word start)
-    (decode 3)
+    (coding/decode 3)
     (wherever :pitch, :pitch temperament/equal)
     (where :time (bpm 120))
     (where :duration (bpm 120))))
