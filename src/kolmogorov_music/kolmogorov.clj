ef29d8a87d95d1c1de1d788735820abe527b43a2
Chris Ford <christophertford@gmail.com>
2015-09-13 13:23:02 +0100

    Move Row-Row into the main talk file.

--- src/kolmogorov_music/kolmogorov.clj
+++ src/kolmogorov_music/kolmogorov.clj
@@ -3,6 +3,7 @@
             [midje.sweet :refer :all :exclude [after]]
             [overtone.live :refer :all]
             [leipzig.melody :refer :all]
+            [leipzig.canon :as canon]
             [leipzig.scale :as scale]
             [leipzig.live :as live]
             [leipzig.chord :as chord]
@@ -49,6 +50,30 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Row, row, row your boat ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def row-row "A simple melody built from durations and pitches."
+              ; Row, row, row  your boat,
+  (->> (phrase [3/3  3/3  2/3  1/3  3/3]
+               [  0    0    0    1    2])
+    (then
+              ; Gent-ly  down the  stream,
+       (phrase [2/3  1/3  2/3  1/3  6/3]
+               [  2    1    2    3    4]))
+    (then
+              ; Merrily, merrily, merrily, merrily,
+       (phrase (repeat 12 1/3)
+               (mapcat (partial repeat 3) [7 4 2 0])))
+    (then
+             ; Life  is   but  a    dream!
+       (phrase [2/3  1/3  2/3  1/3  6/3]
+               [  4    3    2    1    0]))
+    (where :part (is :leader))))
+
+(comment
+  (->> row-row
+    (canon/canon (canon/simple 4))
+    (where :pitch (comp scale/A scale/major))
+    live/play)) 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; The Library of Babel ;;;
