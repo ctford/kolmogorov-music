de0b6c1bc68185d08d1b6536bdfd59a20d380688
Chris Ford <christophertford@gmail.com>
2015-09-13 12:52:12 +0100

    Only use the number in the main file.

---
+++ src/kolmogorov_music/blurred_lines.clj
@@ -0,0 +1,24 @@
+(ns kolmogorov-music.blurred-lines
+  (:require [leipzig.melody :refer :all]
+            [leipzig.scale :as scale]
+            [leipzig.chord :as chord]
+            [leipzig.temperament :as temperament]
+            [kolmogorov-music.coding :as coding]))
+
+(def blurred-lines
+  (let [riff (phrase (repeat 8 1/2) (concat [[nil nil]] (repeat 6 [2 4]) [[nil nil]]))
+        accompaniment (->> (times 4 riff)
+                           (then (times 3 (wherever :pitch, :pitch (scale/from -3) riff)))
+                           (then (phrase [4] [[nil nil]])))
+        bass
+        (->>
+          (phrase [1/2 6/2 1/2 1/2 6/2 1/2 1/2 6/2 1/2 1/2 6/2 1/2]
+                  (cycle [-14 nil -14]))
+          (then
+            (phrase [1/2 6/2 1/2 1/2 6/2 1/2 1/2 6/2 1/2 1/2 1/2 1/2 1/2 1/2 1/2 1/2 1/2]
+                    [-17 nil -17 -17 nil -17 -17 nil -17 -10 -3 -11 -4 -12 -5 -13 -14])))
+        harmony (phrase [32] [14])]
+    (->>
+      (with bass accompaniment)
+      (wherever :pitch, :pitch (comp scale/A scale/major))
+      coding/code)))
--- src/kolmogorov_music/instrument.clj
+++ src/kolmogorov_music/instrument.clj
@@ -7,7 +7,7 @@
       (+ (* 1/3 (sin-osc (* 2.01 freq))))
       (+ (* 1/8 (sin-osc (* 5.01 freq))))
       (+ (* 1/2 (sin-osc (* 1/2 freq))))
-      (rlpf (line:kr 3000 0 dur) 1/5)
+      (rlpf (line:kr 4000 freq dur) 1/3)
       (clip2 0.7)
       (* (env-gen (adsr 0.001 0.03 0.9 0.2) (line:kr 1 0 dur) :action FREE))
       (* vol)))
