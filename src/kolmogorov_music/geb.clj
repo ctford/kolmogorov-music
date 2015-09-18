3cc27270902aee5b6922f7a38d75800bd8df5646
Chris Ford <christophertford@gmail.com>
2015-09-18 22:00:20 +0100

    Tidied up GEB.

--- src/kolmogorov_music/geb.clj
+++ src/kolmogorov_music/geb.clj
@@ -10,47 +10,38 @@
             [kolmogorov-music.coding :as coding]))
 
 (def geb
-  (let [timing [4 4 8]
-        theme (->> "GEB"
+  (let [theme (->> "GEB"
                    (map coding/char->ascii)
-                   (phrase timing)
+                   (phrase [4 4 8])
                    (canon/canon #(where :pitch coding/ascii->midi %)))
-        bass (->> (phrase timing [-2 -1 0])
-                  (where :pitch (comp scale/lower scale/lower)))
+        bass (phrase [4 4 8] [-2 -1 0])
+        bass2 (phrase (repeat 4 4) (cycle [3 0]))
         decoration (phrase (repeat 64 1/4) (cycle [7 8 9 11 7 6]))
-        arpeggios (->>
-                    [-2 -1 0 0]
-                    (mapthen
-                    #(->> (phrase (repeat 1/2) [[0 2] -3 [0 2] -3 [0 3] -3 [0 2]]) (where :pitch (scale/from %)))
-                    )
-                    (then (phrase (repeat 1/2) [[0 3] -3 [0 2] -3]))
-                    )
-        fiddle (phrase (repeat 32 1/2) (cycle [4 2 2 0 -1]))
-        fiddle2 (phrase (repeat 64 1/4) (cycle [4 2 5 4 5 4 7 7]))
-        bass2 (phrase (repeat 4) [-11 -14 -11 -14])
-        ]
+        grind (->> [-2 -1 0 0]
+                   (mapthen #(->> (phrase (repeat 7 1/2) (interleave [[0 2] [0 2] [0 3] [0 2]] (repeat -3)))
+                                  (where :pitch (scale/from %))))
+                   (then (phrase (repeat 4 1/2) (interleave [[0 3] [0 2]] (repeat -3)))))
+        twiddle (with (phrase (repeat 32 1/2) (cycle [4 2 2 0 -1])) (phrase (repeat 64 1/4) (cycle [4 2 5 4 5 4 7 7])))]
     (->> bass
-         ;(with fiddle fiddle2)
+         (where :pitch (comp scale/lower scale/lower))
+         ;(with twiiddle)
          ;(with decoration)
-         ;(with arpeggios)
+         ;(with grind)
          (where :pitch (comp scale/B scale/minor))
          (with theme)
          (where :time (bpm 90))
          (where :duration (bpm 90)))))
 
 (comment
-  (live/stop)
   (fx-chorus)
-  (fx-reverb)
-  (fx-distortion)
   (live/jam (var geb))
   )
 
 ; Instrumentation
 (definst overchauffeur [freq 110 dur 1.0 vol 1.0]
   (-> (sin-osc freq)
-      (+ (* 1/2 (sin-osc (* 3.01 freq))))
       (+ (* 1/3 (sin-osc (* 2.01 freq))))
+      (+ (* 1/2 (sin-osc (* 3.01 freq))))
       (+ (* 1/8 (sin-osc (* 5.01 freq))))
       (+ (* 2 (sin-osc (* 0.5 freq))))
       (clip2 0.7)
