60a1414f7b7a4fe63a773f6e639a464ed8edac67
Chris Ford <christophertford@gmail.com>
2015-09-20 20:38:22 +0100

    Fix indentation on row-row lyrics.

--- src/kolmogorov_music/talk.clj
+++ src/kolmogorov_music/talk.clj
@@ -73,19 +73,18 @@
 
 (def row-row
   "A simple melody built from durations and pitches."
-  ; Row, row, row  your boat,
+              ; Row, row, row  your boat,
   (->> (phrase [3/3  3/3  2/3  1/3  3/3]
                [  0    0    0    1    2])
        (then
-         ; Gent-ly  down the  stream,
+                ; Gent-ly   down the  stream,
          (phrase [2/3  1/3  2/3  1/3  6/3]
                  [  2    1    2    3    4]))
-       (then
-         ; Merrily, merrily, merrily, merrily,
+       (then    ; Merrily, merrily, merrily, merrily,
          (phrase (repeat 12 1/3)
                  (mapcat (partial repeat 3) [7 4 2 0])))
        (then
-         ; Life  is   but  a    dream!
+                ; Life is   but  a    dream!
          (phrase [2/3  1/3  2/3  1/3  6/3]
                  [  4    3    2    1    0]))
        (canon/canon (canon/simple 4))
