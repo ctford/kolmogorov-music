0a2939c7a166b322a74edfb14bde540199a990a0
Chris Ford <christophertford@gmail.com>
2015-09-24 13:13:15 +0100

    Name it the riff, as that's what I always refer to it as.

--- src/kolmogorov_music/geb.clj
+++ src/kolmogorov_music/geb.clj
@@ -20,18 +20,18 @@
         bass (phrase [4 4 8] [-2 -1 0])
         bass2 (phrase (repeat 4 4) (cycle [3 0]))
         decoration (phrase (repeat 64 1/4) (cycle [7 8 9 11 7 6]))
-        grind (->> [-2 -1 0 0]
-                   (mapthen #(->> (phrase (repeat 7 1/2)
-                                          (interleave [[0 2] [0 2] [0 3] [0 2]]
-                                                      (repeat -3)))
-                                  (where :pitch (scale/from %))))
-                   (then (phrase (repeat 4 1/2) (interleave [[0 3] [0 2]] (repeat -3)))))
+        riff (->> [-2 -1 0 0]
+                  (mapthen #(->> (phrase (repeat 7 1/2)
+                                         (interleave [[0 2] [0 2] [0 3] [0 2]]
+                                                     (repeat -3)))
+                                 (where :pitch (scale/from %))))
+                  (then (phrase (repeat 4 1/2) (interleave [[0 3] [0 2]] (repeat -3)))))
         twiddle (with (phrase (repeat 32 1/2) (cycle [4 2 2 0 -1])) (phrase (repeat 64 1/4) (cycle [4 2 5 4 5 4 7 7])))]
     (->> bass
          (where :pitch (comp scale/lower scale/lower))
          ;(with twiddle)
          ;(with decoration)
-         ;(with grind)
+         ;(with riff)
          (where :pitch (comp scale/B scale/minor))
          (with theme)
          (tempo (bpm 90)))))
