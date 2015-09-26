a53a58f37450ab77533fb24dc5e1d2e344285fa0
Chris Ford <christophertford@gmail.com>
2015-09-26 05:57:04 +0100

    Add a hpf to avoid damaging any speakers.

--- src/kolmogorov_music/geb.clj
+++ src/kolmogorov_music/geb.clj
@@ -51,6 +51,7 @@
       (+ (* 2 (sin-osc (* 0.5 freq))))
       (clip2 0.7)
       (lpf top)
+      (hpf 20)
       (* (env-gen (adsr 0.01 0.2 0.8 0.2) (line:kr 1 0 dur) :action FREE))
       (* vol)))
 
