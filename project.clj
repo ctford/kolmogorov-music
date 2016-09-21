015126f24558f6e0846892880f0fcd706f429d9c
Chris Ford <christophertford@gmail.com>
2016-09-21 18:18:59 +0100

    Update Clojure and Leipzig.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject kolmogorov-music "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [overtone "0.9.1"]
-                 [leipzig "0.9.0"]]
+                 [leipzig "0.10.0"]]
   :profiles {:dev
              {:plugins [[lein-midje "3.1.3"]]
               :dependencies [[midje "1.6.3"]]}})
